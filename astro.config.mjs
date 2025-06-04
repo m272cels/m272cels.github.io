208dc3856f9258024557b72aed6cbb64f93603ce
Kelvin Lu <kelvin.lu.sj@gmail.com>
2025-06-04 01:57:44 -0700

    build: Set up GitHub Pages deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,4 +2,10 @@
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: 'https://m272cels.github.io',
+  base: '/',
+  build: {
+    format: 'directory'
+  }
+});
